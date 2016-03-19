f1a0f744f09b47d6193cbcadc48d1bab4cd4fd07
YGskty <zhw793984895@163.com>
2016-03-19 15:56:51 +0800

    first commit

---
+++ build/pionner3_base/CMakeFiles/rosaria_generate_messages_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/rosaria_generate_messages_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/rosaria_generate_messages_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
